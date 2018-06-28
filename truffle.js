aabf82c0d10deea7d83071838e741c55a53bfab6
Ami Kehaty <amiramke@gmail.com>
2018-06-28 09:26:05 -0700

    add id to data mapping

--- truffle.js
+++ truffle.js
@@ -3,8 +3,8 @@ module.exports = {
     'ganache': {
       host: 'localhost',
       port: 7545,
-      network_id: '577', // Ropsten ID 3
-      from: '0xEFf34356F1Ed3029731c6e8CBEF5d76fbF70862e',
+      network_id: '5777', // Ropsten ID 3
+      from: '0xd7506989bce1F4c2291a486f0499c8286406B3c7',
       gas: 4000000
     },
   },
